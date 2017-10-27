e604111303d0c1380b8e389a720132ae36d8f7b6
dhguswn <30751310+dhguswn@users.noreply.github.com>
2017-10-27 22:16:36 +0900

    Revert "Merge branch 'master' of https://github.com/sam32809/Creative-Engineering-Design"
    This reverts commit 900dbc9fe4a212a18477441486dad37b1584bed6, reversing
    changes made to c3735ecde0ce7507ac2e6e283b01fc4300495489.

--- SAM/test123/test123.ino
+++ SAM/test123/test123.ino
@@ -1,21 +1,9 @@
 void setup() {
- pinMode(2,OUTPUT);
- pinMode(3,INPUT);
- pinMode(4,INPUT); 
- pinMode(5,INPUT); 
+  // put your setup code here, to run once:
 
 }
 
 void loop() {
- if (digitalRead(3)==HIGH){
-  tone(2,329.6);
- }
- else if (digitalRead(4)==HIGH){
-  tone(2, 361.6);
- }
- else if (digitalRead(5)==HIGH){
- tone(2, 440.0);
-else{
-  noTone(2);
-}
+  // put your main code here, to run repeatedly:
+
 }
